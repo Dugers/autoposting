0aafd4790e9940fa0eb563398ff36442a080474b
unknown <opka4444@gmail.com>
2022-07-12 21:41:49 +0700

    Add new dp

--- filters/users.py
+++ filters/users.py
@@ -4,7 +4,13 @@ from aiogram.dispatcher.filters import BoundFilter
 
 
 class OnlyAdminFilter(BoundFilter):
+    def __init__(self, reverse=False):
+        self.reverse = reverse
     async def check(self, message: Message):
         if message.from_user.id in ADMINS_IDS:
+            if self.reverse:
+                return False
+            return True
+        if self.reverse:
             return True
         return False
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -3,6 +3,12 @@ from filters import OnlyAdminFilter
 from keyboards.default import main_menu_keyboard
 from aiogram.types import Message
 
+
+@dp.message_handlers(OnlyAdminFilter(reverse=True))
+async def not_admin_message(message: Message):
+    await message.answer("У вас недостаточно прав для выполнения этой команды")
+
+
 @dp.message_handler(OnlyAdminFilter(), commands=['start'])
 async def start(message: Message):
     await message.answer("Добавляй группы\nСобирай из них данные\nДобавляй собранные данные в свой канал", reply_markup=main_menu_keyboard)
